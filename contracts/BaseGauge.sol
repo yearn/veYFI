f712ba23be937958400fbb2eb29e4b3ab7fbc517
panda <87183122+pandadefi@users.noreply.github.com>
2022-03-15 18:25:44 +0100

    fix: remove need for oz upgradeable

--- contracts/BaseGauge.sol
+++ contracts/BaseGauge.sol
@@ -3,10 +3,11 @@ pragma solidity 0.8.12;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import "./interfaces/IBaseGauge.sol";
 
-abstract contract BaseGauge is IBaseGauge, OwnableUpgradeable {
+abstract contract BaseGauge is IBaseGauge, Ownable, Initializable {
     IERC20 public override rewardToken;
     //// @notice rewards are distributed during 7 days when queued.
     uint256 public duration;
