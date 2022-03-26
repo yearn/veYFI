f2f6fae908dc2e0f6f39da60afcede65bb384875
panda <87183122+pandadefi@users.noreply.github.com>
2022-03-26 07:57:20 +0100

    test: add testing with 1000 delegates

--- tests/functional/test_vote_delegation.py
+++ tests/functional/test_vote_delegation.py
@@ -98,3 +98,22 @@ def test_delegate_gas_100(
     vote_delegation.delegate(whale, {"from": accounts[109]})  # gas used: 269476
 
     accounts = accounts[:10]
+
+
+@pytest.mark.skip(reason="really slows down the test suite")
+def test_delegate_gas_1000(
+    accounts, yfi, ve_yfi, ve_yfi_rewards, whale, panda, fish_amount, vote_delegation
+):
+    for i in range(1000):
+        accounts.add()
+        yfi.mint(accounts[-1], fish_amount)
+        yfi.approve(ve_yfi, fish_amount, {"from": accounts[-1]})
+        ve_yfi.create_lock(
+            fish_amount, chain.time() + 3600 * 24 * 365, {"from": accounts[-1]}
+        )
+
+    for i in range(11, 1010):
+        vote_delegation.delegate(panda, {"from": accounts[i]})
+
+    tx = vote_delegation.removeDelegation({"from": accounts[1009]})
+    assert tx.gas_usage < 2_000_000
