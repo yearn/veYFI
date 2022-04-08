31cdbf5801631b1b7c2158a52ad5fdc92440039c
panda <87183122+pandadefi@users.noreply.github.com>
2022-04-08 15:16:30 +0200

    feat: test deploy script

---
+++ scripts/deploy.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from readline import append_history_file
+
+import click
+from ape import accounts, project, chain
+from ape.cli import NetworkBoundCommand, network_option, account_option
+
+
+@click.group(short_help="Deploy the project")
+def cli():
+    pass
+
+
+@cli.command(cls=NetworkBoundCommand)
+@network_option()
+@account_option()
+def main(network, account):
+    yfi = account.deploy(project.Token, "yfi")
+    # deploy veYFI
+    ve_yfi = account.deploy(project.VotingEscrow, yfi, "veYFI", "veYFI")
+    ve_yfi_rewards = account.deploy(
+        project.VeYfiRewards, ve_yfi, chain.pending_timestamp, yfi, account, account
+    )
+    ve_yfi.set_reward_pool(ve_yfi_rewards, sender=account)
+
+    # deploy gauge Factory
+    gauge = account.deploy(project.Gauge)
+    extra_reward = account.deploy(project.ExtraReward)
+    gauge_factory = account.deploy(project.GaugeFactory, gauge, extra_reward)
+
+    # deploy gauge registry
+    registry = account.deploy(
+        project.Registry, ve_yfi, yfi, gauge_factory, ve_yfi_rewards
+    )
+
+    # deploy a vault
+    token = account.deploy(project.Token, "test token")
+    vault = account.deploy(project.dependencies["vault"].Vault)
+    vault.initialize(token, account, account, "", "", sender=account)
+
+    # create gauge
+    tx = registry.addVaultToRewards(vault, account, account, sender=account)
