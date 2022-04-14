a90529d3268025604de61d2d5abbad627a3e441e
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2022-04-13 21:19:27 -0500

    refactor: update fixture scoping

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -3,68 +3,68 @@ import pytest
 from eth_utils import to_checksum_address
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def gov(accounts):
     yield accounts[0]
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def whale_amount():
     yield 10**22
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def whale(accounts, yfi, whale_amount):
     a = accounts[1]
     yfi.mint(a, whale_amount, sender=a)
     yield a
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def shark_amount():
     yield 10**20
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def shark(accounts, yfi, shark_amount):
     a = accounts[2]
     yfi.mint(a, shark_amount, sender=a)
     yield a
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def fish_amount():
     yield 10**18
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def fish(accounts, yfi, fish_amount):
     a = accounts[3]
     yfi.mint(a, fish_amount, sender=a)
     yield a
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def panda(accounts):
     yield accounts[4]
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def doggie(accounts):
     yield accounts[5]
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def bunny(accounts):
     yield accounts[6]
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def yfi(project, gov):
     yield gov.deploy(project.Token, "YFI")
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def create_token(project, gov):
     def create_token(name):
         return gov.deploy(project.Token, name)
@@ -72,31 +72,31 @@ def create_token(project, gov):
     yield create_token
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def ve_yfi(project, yfi, gov):
     yield gov.deploy(project.VotingEscrow, yfi, "veYFI", "veYFI")
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def ve_yfi_rewards(project, ve_yfi, yfi, gov):
     ve_yfi_rewards = gov.deploy(project.VeYfiRewards, ve_yfi, yfi, gov)
     ve_yfi.set_reward_pool(ve_yfi_rewards, sender=gov)
     yield ve_yfi_rewards
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def gauge_factory(project, gov):
     gauge = gov.deploy(project.Gauge)
     extra_reward = gov.deploy(project.ExtraReward)
     yield gov.deploy(project.GaugeFactory, gauge, extra_reward)
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def registry(project, gov, ve_yfi, yfi, gauge_factory, ve_yfi_rewards):
     yield gov.deploy(project.Registry, ve_yfi, yfi, gauge_factory, ve_yfi_rewards)
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def create_vault(project, gov):
     def create_vault():
         return gov.deploy(project.Token, "Yearn vault")
@@ -104,7 +104,7 @@ def create_vault(project, gov):
     return create_vault
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def create_gauge(registry, gauge_factory, gov, project):
     def create_gauge(vault):
         tx = registry.addVaultToRewards(vault, gov, gov, sender=gov)
@@ -115,7 +115,7 @@ def create_gauge(registry, gauge_factory, gov, project):
     yield create_gauge
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def create_extra_reward(gauge_factory, gov, project):
     def create_extra_reward(gauge, token):
         tx = gauge_factory.createExtraReward(gauge, token, gov, sender=gov)
