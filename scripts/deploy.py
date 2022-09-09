39ae13713c7d0889b36ce21c28baed255f3236f8
pandadefi <87183122+pandadefi@users.noreply.github.com>
2022-09-09 15:04:47 +0200

    Update deploy script (#211)
    * fix: deploy script
    
    * fix: deploy script
    
    * feat: add missing field

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,7 +4,9 @@ from readline import append_history_file
 import click
 from ape import accounts, project, chain
 from ape.cli import NetworkBoundCommand, network_option, account_option
-
+from eth._utils.address import generate_contract_address
+from eth_utils import to_checksum_address, to_canonical_address
+from datetime import datetime
 
 @click.group(short_help="Deploy the project")
 def cli():
@@ -14,34 +16,20 @@ def cli():
 @cli.command(cls=NetworkBoundCommand)
 @network_option()
 @account_option()
-def main(network, account):
-    yfi = account.deploy(project.Token, "yfi")
+def deploy_veyfi(network, account):
+    now = datetime.now()
+    yfi = "0x6bD8a96197bfe16a2BA8e492318CcbA655A74077" # Testnet
     # deploy veYFI
-    ve_yfi = account.deploy(project.VotingYFI, yfi, "veYFI", "veYFI")
-
-    # Only when we have reached a new 2 week period we can deploy what's left, make sure some veYFI has been locked.
-    begining_of_week = int(chain.pending_timestamp / 14 * 86400) * 14 * 86400
-    assert ve_yfi.totalSupply(begining_of_week) > 0
-
-    ve_yfi_rewards = account.deploy(
-        project.VeYfiRewards, ve_yfi, chain.pending_timestamp, yfi, account, account
+    reward_pool_address = to_checksum_address(
+        generate_contract_address(
+            to_canonical_address(str(account)), account.nonce + 1
+        )
     )
-    ve_yfi.set_reward_pool(ve_yfi_rewards, sender=account)
-
-    # deploy gauge Factory
-    gauge = account.deploy(project.Gauge)
-    extra_reward = account.deploy(project.ExtraReward)
-    gauge_factory = account.deploy(project.GaugeFactory, gauge, extra_reward)
-
-    # deploy gauge registry
-    registry = account.deploy(
-        project.Registry, ve_yfi, yfi, gauge_factory, ve_yfi_rewards
-    )
-
-    # deploy a vault
-    token = account.deploy(project.Token, "test token")
-    vault = account.deploy(project.dependencies["vault"].Vault)
-    vault.initialize(token, account, account, "", "", sender=account)
-
-    # create gauge
-    tx = registry.addVaultToRewards(vault, account, account, sender=account)
+    veyfi = account.deploy(project.VotingYFI, yfi, reward_pool_address, required_confirmations=0)
+    start_time = (
+        int(datetime.timestamp(now)) + 7 * 3600 * 24
+    )  # MUST offset by a week otherwise token distributed are lost since no lock has been made yet.
+    reward_pool = account.deploy(project.RewardPool, veyfi, start_time, required_confirmations=0)
+    print(reward_pool)
+    print(reward_pool_address)
+    assert str(reward_pool) == reward_pool_address, "broken setup"
