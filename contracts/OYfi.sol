02b5239314f25d9c0fc9b76d4e89716df0c8df2b
pandadefi <87183122+pandadefi@users.noreply.github.com>
2023-02-10 09:26:47 +0000

    feat: add oYFI minter operator

--- contracts/OYfi.sol
+++ contracts/OYfi.sol
@@ -5,9 +5,18 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract OYfi is ERC20, Ownable {
+    mapping(address => bool) public minters;
+    event MinterUpdated(address minter, bool allowed);
+
     constructor() ERC20("OYFI", "OYFI") {}
 
-    function mint(address _to, uint256 _amount) external onlyOwner {
+    function setMinter(address _minter, bool _allowed) external onlyOwner {
+        minters[_minter] = _allowed;
+        emit MinterUpdated(_minter, _allowed);
+    }
+
+    function mint(address _to, uint256 _amount) external {
+        assert(minters[msg.sender]);
         _mint(_to, _amount);
     }
 
